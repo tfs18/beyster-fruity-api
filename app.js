d3fd8552ad8cc687729c18c904b0982a57b1a655
tfs18 <tfitzsmith1@gmail.com>
2025-08-12 10:21:58 +0100

    Add cors to api

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 
-app.use(express.json())
-
 const logger = require("./logger")
-
 const { fruitsRouter } = require("./routes/fruits")
 
+app.use(cors())
+app.use(express.json())
 app.use(logger)
+
 app.use("/fruits", fruitsRouter)
 
 module.exports = {
--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const { app } = require("./app")
 const port = process.env.PORT
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`FruityAPI running on port ${port}`);
 })
